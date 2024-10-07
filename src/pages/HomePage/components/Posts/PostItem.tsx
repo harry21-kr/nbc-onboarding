b43b88ad5d9bb5b47822af6a688ed942c4654f17
박원빈 <harry21@kakao.com>
2024-10-08 00:03:22 +0900

    (design) PostItem: change layout

--- src/pages/HomePage/components/Posts/PostItem.tsx
+++ src/pages/HomePage/components/Posts/PostItem.tsx
@@ -7,8 +7,8 @@ interface PostItemProps {
 const PostItem = ({ post }: PostItemProps) => {
   const { body, title, id } = post;
   return (
-    <li className="py-4">
-      <h3 className="text-lg font-bold">
+    <li className="py-4 break-words max-w-[600px] border-b border-b-gray-500">
+      <h3 className="text-lg font-bold mb-2">
         {id}. {title}
       </h3>
       <p>{body}</p>
